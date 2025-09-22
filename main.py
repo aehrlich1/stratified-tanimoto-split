d2a10e0fe42950781b41e2d46f960abf14df5412
aehrlich1 <anatol.ehrlich@gmail.com>
2025-09-22 17:02:14 +0200

    Added stratified KFold dataset splitting

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import argparse
 
+import pandas as pd
+import torch
+from sklearn.model_selection import StratifiedKFold
+from torch import nn
+from torch.optim import Adam
+from torch_geometric.loader import DataLoader
+
 from data import PolarisDataset
+from models import GINModel
 
 
 def main(params: dict):
-    pass
     # Load polaris dataset for a specific endpoint ("MLM", "HLM", etc.)
     # Split according to predefined Time split: -> train_dataset, test_dataset
     train_dataset = PolarisDataset(root="./dataset", task="MLM", train=True, force_reload=True)
@@ -15,12 +22,60 @@ def main(params: dict):
 
     # Split train dataset into K-Fold Cross validation according to: {Stratified, Scaffold, STS}
     # -> train_fold, valid_fold
+    smiles = train_dataset.smiles
+    labels = train_dataset.y.view(-1).tolist()
+
+    y_binned = pd.qcut(labels, q=10, labels=False)
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+    # Initialize model and loss_fn
+    model = GINModel(hidden_channels=32, out_channels=64, num_layers=3, dropout=0.1, encoding_dim=8)
+    loss_fn = nn.L1Loss()
+    optimizer = Adam(model.parameters(), lr=0.001, weight_decay=1e-4)
+
+    for train_idx, valid_idx in skf.split(smiles, y_binned):
+        train_fold = train_dataset[train_idx]
+        valid_fold = train_dataset[valid_idx]
+
+        train_fold_dataloader = DataLoader(train_fold, batch_size=32, shuffle=True)
+        valid_fold_dataloader = DataLoader(valid_fold, batch_size=32, shuffle=False)
+
+        train_loop(train_fold_dataloader, model, loss_fn, optimizer)
+        test_loop(valid_fold_dataloader, model, loss_fn)
 
     # Train the GIN-E model with a set of hyperparameters on the train and valid folds
     # Take the average value accross all MAE validations and choose the best one
 
     # Choose the hyperparameters with the lowest average MAE and retrain on the entire train dataset
     # Evaluate on the test dataset, and get an MAE score
+
+
+def train_loop(dataloader, model, loss_fn, optimizer):
+    model.train()
+
+    epoch_loss = 0
+
+    for data in dataloader:
+        out = model(data)
+        loss = loss_fn(out, data.y)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        epoch_loss += loss.item()
+
+    return epoch_loss
+
+
+def test_loop(dataloader, model, loss_fn):
+    model.eval()
+    test_loss = 0
+
+    with torch.no_grad():
+        for data in dataloader:
+            out = model(data)
+            test_loss += loss_fn(out, data.y)
+
+    return test_loss
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -1,8 +1,9 @@
-"""
-This file will contain all our models
-"""
+import sys
 
+import torch
+import torch_geometric.utils.smiles as pyg_smiles
 from torch import nn
+from torch_geometric.nn import GIN
 from torch_geometric.nn.pool import global_add_pool
 
 
@@ -28,10 +29,83 @@ class GINModel(nn.Module):
         )
         self.pool = global_add_pool
 
+        self.projection = nn.Sequential(
+            nn.Linear(out_channels, out_channels * 2),
+            nn.ReLU(),
+            nn.Linear(out_channels * 2, out_channels * 2),
+            nn.ReLU(),
+            nn.Linear(out_channels * 2, 1),
+        )
+
     def forward(self, data):
         data = self.encoding_model(data)
         x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
         h = self.model(x=x, edge_index=edge_index, edge_attr=edge_attr)
         h_G = self.pool(x=h, batch=data.batch)
+        out = self.projection(h_G)
 
-        return h_G
+        return out
+
+
+class CategoricalEncodingModel(nn.Module):
+    def __init__(self, embedding_dim):
+        super().__init__()
+        self.node_embedding = CategoricalEmbeddingModel(
+            category_type="node", embedding_dim=embedding_dim
+        )
+        self.edge_embedding = CategoricalEmbeddingModel(
+            category_type="edge", embedding_dim=embedding_dim
+        )
+
+    def forward(self, data):
+        data.x = self.node_embedding(data.x)
+        data.edge_attr = self.edge_embedding(data.edge_attr)
+
+        return data
+
+    def get_feature_embedding_dim(self):
+        return self.node_embedding.get_node_feature_dim()
+
+    def get_edge_embedding_dim(self):
+        return self.edge_embedding.get_edge_feature_dim()
+
+
+class CategoricalEmbeddingModel(nn.Module):
+    """
+    Model to embed categorical node or edge features
+    """
+
+    def __init__(self, category_type, embedding_dim=8):
+        super().__init__()
+        if category_type == "node":
+            num_categories = self._get_num_node_categories()
+        elif category_type == "edge":
+            num_categories = self._get_num_edge_categories()
+        else:
+            print("Invalid category type")
+            sys.exit()
+        self.embedding_dim = embedding_dim
+        self.embeddings = nn.ModuleList(
+            [nn.Embedding(num_categories[i], embedding_dim) for i in range(len(num_categories))]
+        )
+
+    def forward(self, x):
+        embedded_vars = [self.embeddings[i](x[:, i]) for i in range(len(self.embeddings))]
+
+        return torch.cat(embedded_vars, dim=-1)
+
+    def get_node_feature_dim(self):
+        return len(self._get_num_node_categories() * self.embedding_dim)
+
+    def get_edge_feature_dim(self):
+        return len(self._get_num_edge_categories() * self.embedding_dim)
+
+    @staticmethod
+    def _get_num_node_categories() -> list[int]:
+        return [
+            len(pyg_smiles.x_map[prop]) for prop in pyg_smiles.x_map
+        ]  # [119, 9, 11, 12, 9, 5, 8, 2, 2]
+
+    @staticmethod
+    def _get_num_edge_categories() -> list[int]:
+        return [len(pyg_smiles.e_map[prop]) for prop in pyg_smiles.e_map]  # [22, 6, 2]
